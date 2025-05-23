2439770672102f4f36785f836aa80cc653a4d557
ddunai <dunai.dan.a@gmail.com>
2025-05-23 16:11:03 +0300

    improve application main test

--- src/test/java/playground/cashcard/CashCardApplicationTests.java
+++ src/test/java/playground/cashcard/CashCardApplicationTests.java
@@ -2,12 +2,16 @@ package playground.cashcard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CashCardApplicationTests {
 
 	@Test
-	void contextLoads() { // Noncompliant - method is empty
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 
 }
