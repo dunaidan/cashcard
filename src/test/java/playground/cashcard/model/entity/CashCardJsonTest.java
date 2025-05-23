6ca1ad6237a7f0366f579862fd809899f84e0b10
ddunai <dunai.dan.a@gmail.com>
2025-05-23 16:26:36 +0300

    refactor CashCardJsonTest

--- src/test/java/playground/cashcard/model/entity/CashCardJsonTest.java
+++ src/test/java/playground/cashcard/model/entity/CashCardJsonTest.java
@@ -1,10 +1,9 @@
-package playground.cashcard;
+package playground.cashcard.model.entity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
-import playground.cashcard.model.entity.CashCard;
 
 import java.io.IOException;
 
