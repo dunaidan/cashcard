9d39145f0e6ee7deb820a9956f6314478da43d99
ddunai <dunai.dan.a@gmail.com>
2025-05-27 16:06:01 +0300

    add delete method in CashCardController

--- src/main/java/playground/cashcard/repository/CashCardRepository.java
+++ src/main/java/playground/cashcard/repository/CashCardRepository.java
@@ -7,9 +7,6 @@ import org.springframework.stereotype.Repository;
 import playground.cashcard.model.entity.CashCard;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
-
 @Repository
 public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
 
